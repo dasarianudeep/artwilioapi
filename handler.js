2a0588ecf39027d433090f62bcc0fc270175f270
anudeepdasari <adasari@ebay.com>
2018-09-20 00:22:56 -0700

    Removed comments

--- handler.js
+++ handler.js
@@ -26,7 +26,4 @@ module.exports.send = (event, context, callback) => {
             console.log(message.sid);
             callback(null, response);
         })
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
